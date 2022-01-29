f95cc1bfd267d10a2c879a0f227c32a369e5eba3
Matteo Luigi Tommasi <75813215+matteo9966@users.noreply.github.com>
2022-01-29 11:00:10 +0100

    Aggiunte squadre 3

--- src/index.js
+++ src/index.js
@@ -4,31 +4,31 @@ const smallestFont = 10;
 const largestFont = 20;
 
 const ListaSquadre1 = [
-  "Lecce - Milan",
+  "1111111Lecce - Milan",
   "UGMK JUNIOR YEKATERINBURG-MBK",
   "ESPANYOL BARCELLONA-RAYO VALLE",
   "UGMK JUNIOR YEKATERINBURG-MBK",
   "BARI-LONDRA",
+  "UGMK JUNIOR YEKAT11111ERINBURG-MBK",
+  "Lecce - Milan",
+  "UGMK JUNIOR YEKATERINBURG-MBK",
+  "ESPANYOL B11111ARCELLONA-RAYO VALLE",
   "UGMK JUNIOR YEKATERINBURG-MBK",
   "Lecce - Milan",
   "UGMK JUNIOR YEKATERINBURG-MBK",
-  "ESPANYOL BARCELLONA-RAYO VALLE",
-  "UGMK JUNIOR YEKATERINBURG-MBK",
-  "Lecce - Milan",
-  "UGMK JUNIOR YEKATERINBURG-MBK",
-  "ESPANYOL BARCELLONA-RAYO VALLE",
-  "UGMK JUNIOR YEKATERINBURG-MBK",
-  "ESPANYOL BARCELLONA-RAYO VALLE",
+  "ESPANYOL BARCELL1111ONA-RAYO VALLE",
+  "UGMK JUNIOR YEKATE1111RINBURG-MBK",
+  "ESPANYOL BARCE111LLONA-RAYO VALLE",
   "UGMK JUNIOR YEKATERINBURG-MBK",
   "Lecce - Milan"
 ];
 
 const ListaSquadre2 = [
   "2 Lecce - QQQMilan",
-  "2 UGMK QQQQJUNIOR YEKATERINBURG-MBKQQQQ",
-  " 2 ESPANYOL BARCELQQQQLONA-RAYO VALLE",
+  "2 UGMK QQQQJUNIOR YEKA111TERINBURG-MBKQQQQ",
+  " 2 ESPANYOL BARCELQQQQLONA-545454RAYO VALLE",
   "2 BARI-LONDRA",
-  " 2 UGMQQQQK JUNIOR YEKATERINBURG-MBK",
+  " 2 UGMQQQQK JUNIO111R YEKATERINBURG-MBK",
   " 2 ESPANQQQYOL BARCELLONA-RAYO VALLE",
   " 2  UGMK JUNIOR YEKATERINBURG-MBK",
   " 2 ESPANYOL BARCELLONA-RAYO VALLE",
@@ -41,6 +41,24 @@ const ListaSquadre2 = [
   " 2  UGMK JUNIOR YEKATERINBURG-MBK"
 ];
 
+const ListaSquadre3 = [
+  "3 Lecce - QQQMilan",
+  "3 UGMK3333333333 QQQQJUNIOR YEKA111TERINBURG-MBKQQQQ",
+  " 3 ESPANYOL33333 BARCELQQQQLONA-545454RAYO VALLE",
+  "3 BARI-LONDRA",
+  " 3 UGMQQQQK JUNIor",
+  " 3 ESPANQQQYOL 33333333333333333333333333333333333333333E",
+  " 3  UGMK JUNIOR YEKATERINBURG-MBK",
+  " 3 ESPANYOL BARCELLONA-RAYO VALLE",
+  " 3 UGMK JUNIOR YEKATERINBURG-MBK",
+  "3 Lecce - Milan",
+  "3 UGMK JUNIOR YEKATERINBURG-MBK",
+  " 3 ESPANYOL BARCELLONA-RAYO VALLE",
+  " 3 UGMK JUNIOR YEKATERINBURG-MBK",
+  " 3 ESPANYOL BARCELLONA-RAYO VALLE",
+  " 3  UGMK JUNIOR YEKATERINBURG-MBK"
+];
+
 //1 conta quanti caratteri entrano in ciascun range di pixel
 
 const containers = document.querySelectorAll(".container");
@@ -48,6 +66,7 @@ const textFieldSquadre = document.querySelectorAll(".wrapper");
 const mainContainer = document.querySelector(".grid");
 const btn1 = document.getElementById("squadre1");
 const btn2 = document.getElementById("squadre2");
+const btn3 = document.getElementById("squadre3");
 const mutationObserver = new MutationObserver((event) => {
   updateInputs();
 });
@@ -86,6 +105,9 @@ btn1.addEventListener("click", (event) => {
 });
 btn2.addEventListener("click", (event) => {
   updateFields(ListaSquadre2);
+});
+btn3.addEventListener("click", (event) => {
+  updateFields(ListaSquadre3);
 });
 
 //questo è un workaround per farlo funzionare su codesandbox! nella vita vera usa l'evento 'load'
